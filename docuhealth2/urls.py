1bc0e81957f3353918a5f21e42e75a1172f1c0bd
Ceejay-777 <covenantcrackslord01@gmail.com>
2025-08-28 13:32:18 +0100

    make redoc root

--- docuhealth2/urls.py
+++ docuhealth2/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('api', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/raw', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger-ui', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('admin', admin.site.urls),
