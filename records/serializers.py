a7ee3772b6874822ace9389847e82146087af97b
Ceejay-777 <covenantcrackslord01@gmail.com>
2026-02-25 22:39:06 +0100

    fix: hospital info in med summary serializer

--- records/serializers.py
+++ records/serializers.py
@@ -359,7 +359,7 @@ class MedicalSummarySerializer(serializers.ModelSerializer):
     
     class Meta:
         model = SoapNote
-        fields = ['patient_info', 'staff_info', 'vital_signs_info', 'chief_complaint', 'primary_diagnosis', 'treatment_plan', 'care_instructions', 'drug_records', 'investigation_docs', 'appointment']
+        fields = ['patient_info', 'staff_info', 'vital_signs_info', 'chief_complaint', 'primary_diagnosis', 'treatment_plan', 'care_instructions', 'drug_records', 'investigation_docs', 'appointment', 'hospital_info']
         read_only_fields = ['id', 'created_at', 'hospital', 'investigation_docs']
     
 class DischargeFormSerializer(MultipartJsonMixin, serializers.ModelSerializer):
